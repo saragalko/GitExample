0b30cfafd6ad49116dfb0afd6b6667c8cfd2ff59
saragalko <155007204+saragalko@users.noreply.github.com>
2024-01-23 17:31:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,134 +1,91 @@
+from dataclasses import dataclass
+
 # task 1
-class Soda:
 
-    def __init__(self, taste,):
-        self.taste = taste
+@dataclass
+class Tovar:
+    name_of_item: str
+    name_of_stor: str
+    item_amount: float
 
-    def my_drink(self, my_drink):
-        self.my_drink = my_drink
+@dataclass
+class Cklad:
+    info_item_index: float
+    info_item_name: str
+    sortirovka_name: str
+    sortirovka_stor: str
+    sortirovka_prise: float
+    summa_item_prise: float
 
-
-my_drink = Soda(taste='Straberry')
-print(my_drink.taste)
-my_drink = Soda(taste='No taste')
-print(my_drink.taste)
 
 # task 2
 
 
-class Math:
-    def __init__(self, a, b, example):
-        self.a = a
-        self.b = b
-        self.example = example
+class BeeElephant:
+    def __init__(self, bee, elephant):
+        self.bee = bee
+        self.elephant = elephant
 
-    def __add__(self):
-        print(self.a + self.b)
+    def fly(self):
+        if self.bee >= self.elephant:
+            return True
+        return False
 
-    def __mul__(self):
-        print(self.a * self.b)
+    def trumpet(self):
+        if self.bee <= self.elephant:
+            return 'tuu-tu-doo-doo'
+        return 'wzzzzz'
 
-    def division(self):
-        print(self.a / self.b)
+    def eat(self, meal, value):
+        if meal == 'nectar':
+            self.bee += value
+            self.elephant -= value
+        elif meal == 'grass':
+            self.bee -= value
+            self.elephant += value
+        if self.elephant > 100:
+            self.elephant = 100
+        elif self.elephant < 0:
+            self.elephant = 0
+        if self.bee > 100:
+            self.bee = 100
+        elif self.bee < 0:
+            self.bee = 0
 
-    def subtraction(self):
-        print(self.a - self.b)
+    def get_parts(self):
+        return (self.bee, self.elephant)
+
 # task 3
 
+class Bus:
+    PAY_COEFF = 0.01
+    MAX_PASSENGERS = 60
 
-class Car:
+    def __init__(self, x, y, direction):
+        super().__init__(x, y, direction)
+        self.passengers = 0
+        self.money = 0
 
-    car_count = 0
+    def move(self, distance):
+        self.money += Bus.PAY_COEFF * self.passengers * distance
+        super().move(distance)
 
-    def __init__(self, color, type, year):
-        self.color = color
-        self.type = type
-        self.year = year
-        Car.car_count += 1
+    def enter(self, passengers):
+        if passengers + self.passengers > Bus.MAX_PASSENGERS:
+            print('Достигнута максимальная вместимость автобуса')
+            print('Уехать смогли только {:d}'.format(Bus.MAX_PASSENGERS - self.passengers))
+            print('Остались {:d}'.format(passengers - (Bus.MAX_PASSENGERS - self.passengers)))
+            passengers = Bus.MAX_PASSENGERS - self.passengers
+        return passengers
 
-    def start(self):
-        print('The car is started')
+    def exit(self, passengers):
+        if passengers > self.passengers:
+            print('Вышли все из автобуса')
+            passengers = self.passengers
+        return passengers
 
-    def stop(self):
-        print('The car is turned off')
-
-    def set_color(self, color):
-        self.color = color
-
-    def set_type(self, type):
-        self.type = type
-
-    def set_year(self, year):
-        self.year = year
-
-
-car_a = Car(color='blue', type='sportcar', year=2005)
-car_a.start()
-print(car_a.car_count)
-car_a.stop()
-
-car_b = Car(color='yellow', type='liftback', year=2017)
-car_b.start()
-print(car_b.car_count)
-
-car_c = Car(color='red', type='family', year=2000)
-car_c.start()
-print(car_c.car_count)
-car_c.stop()
-
-# task 4
-from math import pi
-
-
-class Sphere:
-    def __int__(self, *arg):
-        if len(arg) == 0:
-            arg = (1, 0, 0, 0)
-        elif len(arg) == 1:
-            arg = (arg[0], 0, 0, 0)
-        else:
-            raise TypeError
-        self.r, self.x, self.y, self.z = arg
-
-    def get_volume(self):
-        return (self.r ** 3) * pi * 4/3
-
-    def get_square(self):
-        return (self.r ** 2) * pi * 4
-
-    def get_radius(self):
-        return self.r
-
-    def get_center(self):
-        return (self.x, self.y, self.z)
-
-    def set_radius(self, r):
-        self.r = r
-
-    def set_center(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def is_point_inside(self, x, y, z):
-        return (self.x - x) ** 2 + (self.y - y) ** 2 + (self.z - z) ** 2 < self.r ** 2
-
-
-some = Sphere()
-print(some.get_volume)
-print(some.get_radius)
-print(some.get_center)
-print(some.set_center)
-
-# task 5
-
-
-class SuperStr:
-    def is_repeatance(self, s):
-        if not isinstance(s, str):
-            return False
-        n = len(self) % (len(s) or 1)
-        return self == n * s
-    def is_palindroma(self):
-        s = self.lower()
-        return s == s[::-1]
+    def __str__(self):
+        lines = [super().__str__(),
+                 f'В автобусе {self.passengers} пассажиров',
+                 f'У водителя {round(self.money, 2)} денег']
+        return '\n'.join(lines)
